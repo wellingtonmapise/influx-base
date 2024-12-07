4549b5937a03b91653eb4917f58dc37eedfe9775
wellingtonmapise <wpm44@cornell.edu>
2024-12-07 14:44:38 -0500

    added new node modules

--- influx-app/src/app/(doctorView)/_layout.tsx
+++ influx-app/src/app/(doctorView)/_layout.tsx
@@ -1,14 +1,43 @@
-import { Tabs } from 'expo-router';
+import { Tabs, Redirect } from 'expo-router';
 import React from 'react';
-
 import { TabBarIcon } from '../../components/navigation/TabBarIcon';
-// import { Colors } from '../../constants/Colors';
+import Colors from '@/constants/Colors';
 import { useColorScheme } from '../../../hooks/useColorScheme';
+import { useAuth } from '@/providers/AuthProvider';
 
-export default function StackLayout() {
+export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const { session } = useAuth();
 
+  // if (!session) {
+  //   return <Redirect href={'/'} />;
+  // }
+
+  //Might need to mount auth provider(??)
   return (
-    <Tabs></Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'person' : 'person-outline'} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
