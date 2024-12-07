4068dc9c6f9a21b9df0a3188d3ac791259c75f96
wellingtonmapise <wpm44@cornell.edu>
2024-12-07 14:24:08 -0500

    merged conflicts

--- influx-app/.expo/types/router.d.ts
+++ influx-app/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(auth)` | `/(auth)/sign-in` | `/(auth)/sign-up` | `/(doctorView)` | `/(doctorView)/` | `/(patientView)` | `/(patientView)/` | `/(patientView)/Profile` | `/Profile` | `/_sitemap` | `/sign-in` | `/sign-up`;
+      StaticRoutes: `/` | `/(auth)` | `/(auth)/sign-in` | `/(auth)/sign-up` | `/(doctorView)` | `/(doctorView)/` | `/(doctorView)/Profile` | `/(patientView)` | `/(patientView)/` | `/(patientView)/Profile` | `/Profile` | `/_sitemap` | `/patientView/Profile` | `/sign-in` | `/sign-up`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
